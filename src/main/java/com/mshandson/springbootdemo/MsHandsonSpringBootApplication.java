92e6f066b22f909cb65cb663098e06c58200ee15
subhran <subhranshu497@gmail.com>
2022-09-07 12:15:15 -0700

    initial commit

---
+++ src/main/java/com/mshandson/springbootdemo/MsHandsonSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mshandson.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHandsonSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsHandsonSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mshandson/springbootdemo/MsHandsonSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mshandson.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHandsonSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
